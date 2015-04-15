e090d4a8afbaa50b9d193224a59bc3687b957dc7
Wes Hargrove <wesleyhargrove@gmail.com>
2015-04-14 21:51:26 -0700

    Adds a profiler

--- ReverseWords/Program.cs
+++ ReverseWords/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ReverseWords
 {
@@ -10,8 +9,18 @@ namespace ReverseWords
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Reversing string...");
-			Console.WriteLine(ReverseWords(args[0]));
+			if (args.Length > 1)
+			{
+				Func<string, string> profileReverse = ReverseWords;
+				Console.WriteLine("Using profile...");
+				Profile(50, args[1], profileReverse);
+			}
+			else
+			{
+				Console.WriteLine("Reversing string...");
+				Console.WriteLine(ReverseWords(args[0]));
+			}
+
 		}
 
 		internal static string ReverseWords(string s)
@@ -26,6 +35,27 @@ namespace ReverseWords
 				reversed.Append(string.Format("{0}{1}", split[i], i == 0 ? string.Empty : " "));
 			}
 			return reversed.ToString();
+		}
+
+		static void Profile(int iterations, string input, Func<string, string> reverse)
+		{
+			// Warm up
+			reverse(input);
+
+			var watch = new Stopwatch();
+
+			// Clean up
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+
+			watch.Start();
+			for (int i = 0; i < iterations; i++)
+			{
+				reverse(input);
+			}
+			watch.Stop();
+			Console.WriteLine("Average time per iteration: {0} ms", watch.Elapsed.TotalMilliseconds / iterations);
 		}
 	}
 }
