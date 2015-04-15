093fd330ddd8cb5eafe9c0fae2fe32a2cc11422b
Wes Hargrove <wesleyhargrove@gmail.com>
2015-04-14 21:06:33 -0700

    Initial commit

---
+++ ReverseWords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Reversing string...");
+			Console.WriteLine(ReverseWords(args[0]));
+		}
+
+		internal static string ReverseWords(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+				return string.Empty;
+
+			var reversed = new StringBuilder();
+			var split = s.Split(' ').ToList();
+			for (var i = split.Count - 1; i >= 0; i--)
+			{
+				reversed.Append(string.Format("{0}{1}", split[i], i == 0 ? string.Empty : " "));
+			}
+			return reversed.ToString();
+		}
+	}
+}
---
+++ ReverseWords/ProgramTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace ReverseWords
+{
+	[TestFixture]
+	class ProgramTest
+	{
+		[TestCase("Use the force")]
+		public void ReverseWords_ShouldReturnAString(string s)
+		{
+			// Given a string (see test cases)
+			// When ReverseWords() is invoked
+			var result = Program.ReverseWords(s);
+			// The value of the return type should be a string
+			Assert.That(result, Is.TypeOf(typeof(string)));
+		}
+
+		[TestCase("")]
+		[TestCase(null)]
+		public void ReverseWords_ShouldReturnAnEmptyString(string s)
+		{
+			// Given an empty or null string (see test cases)
+			// When ReverseWords() is invoked
+			var actual = Program.ReverseWords(s);
+			var expected = string.Empty;
+			// Then the output is an empty string
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestCase("A long time ago in a galaxy far far away")]
+		public void ReverseWords_ShouldReturnAStringWithWordsReversed(string s)
+		{
+			// Given a string (see test cases)
+			// When ReverseWords() is invoked
+			var actual = Program.ReverseWords(s);
+			var expected = "away far far galaxy a in ago time long A";
+			// Then the output is the string reversed by word
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
